99cf82b283504414c333c43dd1bc9c30fecc3108
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-09 22:21:55 +0300

    rend

---
+++ Exemple003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(1,1000); // 1 2 3 4 ...999
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,1000);
+Console.WriteLine(numberA);
+Double result = numberA + numberB;
+Console.WriteLine (result);
